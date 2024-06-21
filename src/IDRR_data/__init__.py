0e32e54fce34faf25879556affce3de9f1cec60b
zpwang <zhipangwang@gmail.com>
2024-06-21 20:49:03 +0800

    modified:   src/IDRR_data/__init__.py 	renamed:    src/IDRR_data/show_csv_info.py -> src/IDRR_data/show_df_info.py

--- src/IDRR_data/__init__.py
+++ src/IDRR_data/__init__.py
@@ -2,4 +2,4 @@ from .dataframes import *
 from .dataframes2 import *
 from .dataframes3 import *
 from .fill_prompt import *
-from .show_csv_info import *
+from .show_df_info import *
--- src/IDRR_data/show_df_info.py
+++ src/IDRR_data/show_df_info.py
@@ -1,15 +1,11 @@
 import pandas as pd
 
-from pathlib import Path as path
 
-
-def show_csv_info(csv_path):
-    df = pd.read_csv(csv_path)
-    print('-'*20)
-    print(path(csv_path).stem)
+def show_df_info(df:pd.DataFrame):
     print('total:', len(df))
     print('train:', len(df[df['split']=='train']))
     print('dev  :', len(df[df['split']=='dev']))
     print('test :', len(df[df['split']=='test']))
     print(df.columns)
+    print('-'*20)
     
